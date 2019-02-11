86077ca5c4cc8246e91b04a4723db577aa071475
Margaret Medellin <Margaret.A.Medellin-1@ou.edu>
2019-02-11 11:23:45 -0600

    in class WPF practice-- line 13 not working

---
+++ WPF1/WPF1/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WPF1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        //ef : EntryForm
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string name, address;
+            int zipCode;
+
+            name = txtName.Text;
+            //assigns the string to the textbox 
+            //need .text for string
+            address = txtAddress.Text;
+            zipCode = Convert.ToInt32(txtZipCode.Text);
+            //need to convert then se .text for integers
+            //then add class for constructors
+            bool isFormValid = true;
+            if (name == "" || name ==string.Empty)
+            {
+                MessageBox.Show("Please input your name");
+                txtName.Focus();
+                //return; --dont need if void
+                isFormValid = false;
+            }
+            if (address == "")
+            {
+                MessageBox.Show("Please input your address");
+                isFormValid = false;
+            }
+
+
+
+            entryForm ef = new entryForm();
+            ef.Name = name;
+            ef.Address = address;
+            ef.ZipCode = zipCode;
+            //needs to be in order of text boxes
+            //assigns the value of their input in the class
+
+            string entryForm = $"{name} lives at {address} {zipCode}";
+            _1stApplications.Items.Add(ef);
+
+        }
+
+    
+    }
+}
---
+++ WPF1/WPF1/entryForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF1
+{
+    class entryForm
+    {
+        public string Name { get; set; }
+        //public -- always do get set
+        //private just ;
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+
+        public entryForm()
+        {
+            Name = "";
+            Address = string.Empty;
+            //"" and string.Empty are same
+            ZipCode = 0;
+        }
+
+        public entryForm(string name, string address, int zipCode)
+        {
+            Name = name;
+            //creates row/object/instance
+            Address = address;
+            ZipCode = zipCode;
+
+        }
+
+        public override string ToString()
+        {
+            string entryForm = $"{Name} lives at {Address} {ZipCode}";
+            return entryForm;
+        }
+    }
+}
